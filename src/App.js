4f5df9e09829bac376f944f7d4b608c167e6e5d3
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-07-08 21:57:21 +0200

    revamped ui and switched to dialogflow instead of lex

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ import Auth from "@aws-amplify/auth";
 import Analytics from "@aws-amplify/analytics";
 import Amplify, { Interactions } from "aws-amplify";
 import { ChatBot, AmplifyTheme } from "aws-amplify-react";
+import { ChatFeed, Message } from "react-chat-ui";
+import {
+  Widget,
+  addResponseMessage,
+  addLinkSnippet,
+  addUserMessage,
+  setQuickButtons
+} from "react-chat-widget";
+
+import "react-chat-widget/lib/styles.css";
+
+import profile from "./img/android.jpg";
 
 import awsconfig from "./aws-exports";
+
+import $ from "jquery";
 
 Auth.configure(awsconfig);
 
@@ -16,56 +30,95 @@ Amplify.configure(awsconfig);
 
 Interactions.configure(awsconfig);
 
-const myTheme = {
-  ...AmplifyTheme,
-  sectionHeader: {
-    ...AmplifyTheme.sectionHeader,
-    backgroundColor: "#ff6600"
-  }
-};
-
-// Amplify.configure({
-//   Auth: {
-//     identityPoolId: "eu-west-1:fd4c13e8-455f-4b8a-8303-3fac15b34580",
-//     region: "eu-west-1"
-//   },
-//   Interactions: {
-//     bots: {
-//       VanBot_dev: {
-//         name: "VanBot_dev",
-//         alias: "$LATEST",
-//         region: "eu-west-1"
-//       }
-//     }
-//   }
-// });
-
 class App extends Component {
-  handleComplete(err, confirmation) {
-    if (err) {
-      alert("Bot conversation failed");
-      return;
-    }
+  componentDidMount() {
+    addResponseMessage(
+      "Welcome to Ivan's interactive CV. Please ask anything regarding the owner"
+    );
+  }
 
-    alert("Success: " + JSON.stringify(confirmation, null, 2));
-    return "Trip booked. Thank you! what would you like to do next?";
+  handleNewUserMessage = newMessage => {
+    this.newMessage(newMessage);
+  };
+
+  newMessage(newMessage) {
+    $.ajax({
+      type: "POST",
+      url: "https://api.dialogflow.com/v1/query?v=20170712",
+      contentType: "application/json; charset=utf-8",
+      headers: {
+        Authorization: "Bearer 71adca6fa5be465fa880deed71d1aab8"
+      },
+      data: JSON.stringify({
+        query: newMessage,
+        lang: "en",
+        sessionId: "job-interview-dtlxkt"
+      }),
+      success: function(response) {
+        console.log(response);
+        var messageArr = response.result.fulfillment.messages;
+        for (var i = 0; i < messageArr.length; i++) {
+          addResponseMessage(messageArr[i].speech);
+        }
+      },
+      error: function() {}
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <ChatBot
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
           title="VanBot"
-          theme={myTheme}
-          botName="VanBot_dev"
-          welcomeMessage="Welcome to Ivan's interactive CV. Please ask anything regarding the js master"
-          onComplete={this.handleComplete.bind(this)}
-          clearOnComplete={true}
-          conversationModeOn={true}
+          subtitle="Interactive CV bot"
+          profileAvatar={profile}
         />
       </div>
     );
   }
 }
+
+$(document).ready(function() {
+  $(".rcw-widget-container").addClass("rcw-opened");
+  $(".rcw-launcher").trigger("click");
+  $(".rcw-close-button").remove();
+  $(".rcw-launcher").remove();
+});
+
+const styles = {
+  bubbleStyles: {
+    text: {
+      fontSize: 16
+    },
+    chatbubble: {
+      borderRadius: 30,
+      padding: 10
+    }
+  },
+  headerTitle: {
+    color: "white",
+    fontSize: 22
+  },
+  header: {
+    backgroundColor: "rgb(0, 132, 255)",
+    padding: 20,
+    borderTop: "12px solid rgb(204, 204, 204)"
+  },
+  messagesContainer: {
+    display: "flex",
+    flexDirection: "column",
+    padding: 10,
+    alignItems: "center"
+  },
+  input: {
+    fontSize: 16,
+    padding: 10,
+    outline: "none",
+    width: 350,
+    border: "none",
+    borderBottom: "2px solid rgb(0, 132, 255)"
+  }
+};
 
 export default App;
---
+++ src/env.js
@@ -0,0 +1,12 @@
+export const dialogflowConfig = {
+    "type": "service_account",
+    "project_id": "vanbot-mvaqwx",
+    "private_key_id": "89e9c8c0ae84473b94a5706afe98e03ef9d3242e",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC8zA89H3sJrx46\nnKXGSaqkwqPWQb8qRIDyEQJRI6aJ//i/VYVmk3Ktm/9KJ5rubivFilrbS62G8iX/\npHX79sQWTm2J7AwlMFReGMPEL64FtX/aKxGTZJxD5ZqYyXJlI0tbpMYhFsXKADfa\nXHcvNlELe3FW1GjRoBuyuhSgkdJvKSEN83xhCS6bmx3zG5MhAdXKOhvZTwYl2xlf\nqlD5lmExk1g798EbgwFc8DGNmA9hn1Bv9REbZTv6JAkxQevsEDj+8WLws+nzBhY8\nTV+/Ma46x6N+dGxMMuIi3FqZEqVSyi6U+NyenbBbswKeumlnOQtXv7B4qaEgMTk2\nqZPxClbZAgMBAAECggEAAqDTRCY7CpXN1Ze2qxL9dXu214Bkzowo62BsktVB4paV\n+jvmNoOuS1e/mbhWwiOgfQiBs/R9aZfXYD6u3HTBTWTcG4PAtjJ9Ci5NaLQX4mvL\naC3EpIj4WphIqwCAP4OFRLT/g8MTD85++I2O+ductzc0YGFmmtHKxkCBkawArPF0\neQMHAkWL8qfPm9iHgh4wcRyfOvMcJrIYuJLzeVPkTFBRurZjWzgHCG/kD9CQwRxR\nAawF8XL7CfgriTxr4t8XQ7Cte+tKYmAvEgW/jefwiB87od36WIOOuSwjcAIxEoLy\nNoiPZJWQBOJJjUvBvQS0D5T8y7rOC6BRcIfozUFosQKBgQD2KeAj0sovkb4re72R\n18B4uZOo0EEy/4J5gqaoCx5Gmsk0lTeVROTalxlYKx+SqR8mL8qxiV7O/6QrfZKr\nVRik6E+LOJQ3L7u75XGybmrjL48U+EYssiFcGBcvZ4IVo5roCDv175UBHJ6hQ+uI\nSbP2YClsVYuzcrUlPZCHTwo7hQKBgQDEV1rklZKlkG5pEahzlskcKYMXZk31z8Iz\nLUOff/9dOG23bLoqc706MNzJ8NbLwOYVTXBZu3tMcWo2Kewbp886Q7s8GY4TYfM9\n8N1K1CU7dAtDG8GA1+vDYctzjAWMZoEZzMc13aA51KWDzF06INtMElLo7aJvMYt+\nFPdgrDrcRQKBgGMdsVY7RZWpxTyU8luK18DEWCQc1mPGZa6vYJNBBsu+/0zg2CSO\nPOeW92l805Wl6xjhHWgmu7n1TREF2syJ0wZzU7Pm8CbYXWrmk8UYmIkpWR9EaF0O\nzEfcm1lHACXeXj0XM61u7jwoXebpBik2I/QL/CHWwA1dHnsEMvGIbcJRAoGBALe0\nYDf19tLwU039lpYoJB00fhJ7xz/SVwrZNCdddIo9TPLNSVaFGi0DIOHVdLQJ1X1j\nClb7Av9gMpdhHCg4F72VN++gnX1nLVbsMbFc00VQa2Ur6B5saIxTyS9geKSIGA0S\nu+WbatoRK5gos6W6rlT75BVKmqvdOH4MkfUi0KPdAoGARiwJZ9FTsAhd64ggiaSq\n/hxnMX+42YuUKZeKPWyExOoJLLKffbGJuE3bnaVI4M1JACTLUt5j2p+pfCe/kOEE\n5aVvRAuqYvZoCZWa+P3WL1vzTQPOOT0HcgXe/oehsWiRy6ssjVJo0fQXvTaqoTP3\nSL5BVMmqE3z5QGkN5Yz62rc=\n-----END PRIVATE KEY-----\n",
+    "client_email": "dialogflow-faorow@vanbot-mvaqwx.iam.gserviceaccount.com",
+    "client_id": "113387004625745280124",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-faorow%40vanbot-mvaqwx.iam.gserviceaccount.com"
+  };
