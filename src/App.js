e63ff637387e15fba6e1e42280ea2d4ed63e632b
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2019-08-16 00:22:34 +0200

    Added support for firefox with translattion

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import "./App.css";
 import Auth from "@aws-amplify/auth";
 import Analytics from "@aws-amplify/analytics";
 import Amplify, { Interactions } from "aws-amplify";
-import { ChatBot, AmplifyTheme } from "aws-amplify-react";
-import { ChatFeed, Message } from "react-chat-ui";
 import {
   Widget,
   addResponseMessage,
   addLinkSnippet,
   addUserMessage,
-  setQuickButtons
+  setQuickButtons,
+  renderCustomComponent
 } from "react-chat-widget";
+
+import "jquery-ui";
+
+import "./commercial/font-awesome/css/all.css";
 
 import "react-chat-widget/lib/styles.css";
 
@@ -21,6 +24,8 @@ import profile from "./img/android.jpg";
 import awsconfig from "./aws-exports";
 
 import $ from "jquery";
+
+import Recorder from "opus-recorder";
 
 Auth.configure(awsconfig);
 
@@ -38,87 +43,275 @@ class App extends Component {
   }
 
   handleNewUserMessage = newMessage => {
-    this.newMessage(newMessage);
+    this.detectLang(newMessage);
   };
 
-  newMessage(newMessage) {
+  detectLang(newMessage) {
     $.ajax({
       type: "POST",
-      url: "https://api.dialogflow.com/v1/query?v=20170712",
-      contentType: "application/json; charset=utf-8",
-      headers: {
-        Authorization: "Bearer 71adca6fa5be465fa880deed71d1aab8"
-      },
-      data: JSON.stringify({
-        query: newMessage,
-        lang: "en",
-        sessionId: "job-interview-dtlxkt"
-      }),
+      url:
+        "https://www.googleapis.com/language/translate/v2/detect?key=AIzaSyDJfp0Sztdi_m4skJ1dBSH1JtvVtMYdxlE&q=" +
+        newMessage,
+      dataType: "jsonp",
       success: function(response) {
-        console.log(response);
-        var messageArr = response.result.fulfillment.messages;
-        for (var i = 0; i < messageArr.length; i++) {
-          addResponseMessage(messageArr[i].speech);
-        }
+        var srcLang = response.data.detections[0][0].language;
+        translateText(newMessage, srcLang);
       },
       error: function() {}
     });
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      record: false
+    };
+  }
+
+  onData(recordedBlob) {
+    console.log("chunk of real-time data is: ", recordedBlob);
+  }
+
+  onStop(recordedBlob) {
+    console.log("recordedBlob is: ", recordedBlob);
+    sendAudio(recordedBlob);
+  }
+
+  startRecording = () => {
+    this.setState({
+      record: true
+    });
+  };
+
+  stopRecording = () => {
+    this.setState({
+      record: false
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <Widget
           handleNewUserMessage={this.handleNewUserMessage}
+          startRecording={this.startRec}
+          stopRecording={this.stopRec}
           title="VanBot"
+          mic={true}
           subtitle="Interactive CV bot"
           profileAvatar={profile}
         />
+
+        <audio id="player" controls />
       </div>
     );
   }
 }
 
+function translateText(newMessage, srcLang) {
+  if (srcLang == "en") {
+    createEngMessage(newMessage);
+  } else {
+    $.ajax({
+      type: "POST",
+      url:
+        "https://www.googleapis.com/language/translate/v2?key=AIzaSyDJfp0Sztdi_m4skJ1dBSH1JtvVtMYdxlE&source=" +
+        srcLang +
+        "&target=en&q=" +
+        newMessage,
+      dataType: "jsonp",
+      success: function(response) {
+        var engText = response.data.translations[0].translatedText;
+        createTransMessage(engText, srcLang, newMessage);
+      },
+      error: function() {}
+    });
+  }
+}
+
+async function sendAudio(recordedBlob) {
+  // var arrayBuffer = await new Response(recordedBlob).arrayBuffer();
+  // var floatbuff = arrayBuffer.decodeAudioData(ArrayBuffer);
+  // console.log(floatbuff)
+  let fileReader = new FileReader();
+  let arrayBuffer;
+
+  fileReader.onloadend = () => {
+    arrayBuffer = fileReader.result;
+    console.log(arrayBuffer);
+  };
+
+  fileReader.readAsArrayBuffer(recordedBlob);
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:4242/api/1.0/post",
+    dataType: "arraybuffer",
+    data: {
+      blob: arrayBuffer
+    },
+    success: function(response) {
+      console.log(response);
+    },
+    error: function() {}
+  });
+}
+
+function translateRes(text, srcLang) {
+  $.ajax({
+    type: "POST",
+    url:
+      "https://www.googleapis.com/language/translate/v2?key=AIzaSyDJfp0Sztdi_m4skJ1dBSH1JtvVtMYdxlE&source=en&target=" +
+      srcLang +
+      "&q=" +
+      text,
+    dataType: "json",
+    async: false,
+    success: function(response) {
+      var srcText = response.data.translations[0].translatedText;
+      addResponseMessage(srcText);
+    },
+    error: function() {}
+  });
+}
+
+function createEngMessage(newMessage) {
+  $.ajax({
+    type: "POST",
+    url: "https://api.dialogflow.com/v1/query?v=20170712",
+    contentType: "application/json; charset=utf-8",
+    headers: {
+      Authorization: "Bearer 71adca6fa5be465fa880deed71d1aab8"
+    },
+    data: JSON.stringify({
+      query: newMessage,
+      lang: "en",
+      sessionId: "job-interview-dtlxkt"
+    }),
+    success: function(response) {
+      console.log(response);
+      var messageArr = response.result.fulfillment.messages;
+      if (
+        response.result.metadata.intentName ==
+        "translate.text - context:translate-text - comment:to language"
+      ) {
+        var langTo = response.result.parameters["lang-to"];
+        var text = response.result.parameters["text"];
+      } else {
+        for (var i = 0; i < messageArr.length; i++) {
+          addResponseMessage(messageArr[i].speech);
+        }
+      }
+    },
+    error: function() {}
+  });
+}
+
+function createTransMessage(engText, srcLang, newMessage) {
+  $.ajax({
+    type: "POST",
+    url: "https://api.dialogflow.com/v1/query?v=20170712",
+    contentType: "application/json; charset=utf-8",
+    headers: {
+      Authorization: "Bearer 71adca6fa5be465fa880deed71d1aab8"
+    },
+    data: JSON.stringify({
+      query: engText,
+      lang: "en",
+      sessionId: "job-interview-dtlxkt"
+    }),
+    success: function(response) {
+      console.log(response);
+      var messageArr = response.result.fulfillment.messages;
+      if (
+        response.result.metadata.intentName ==
+        "translate.text - context:translate-text - comment:to language"
+      ) {
+        var langTo = response.result.parameters["lang-to"];
+        var text = response.result.parameters["text"];
+      } else {
+        for (var i = 0; i < messageArr.length; i++) {
+          translateRes(messageArr[i].speech, srcLang);
+        }
+      }
+    },
+    error: function() {}
+  });
+}
+
+function startRecording() {
+  console.log("recording start");
+}
+
 $(document).ready(function() {
+  navigator.mediaDevices
+    .getUserMedia(
+      // constraints - only audio needed for this app
+      {
+        audio: true
+      }
+    )
+
+    // Success callback
+    .then(function(stream) {
+      console.log(navigator.mediaDevices.getSupportedConstraints());
+      var mediaRecorder = new MediaRecorder(stream, { sampleRate: 44100 });
+      var chunks = [];
+      $(".rec-button")
+        .mousedown(function() {
+          console.log("rec start");
+          $(".rec-button i").addClass("recStart");
+          mediaRecorder.start();
+          console.log(mediaRecorder.state);
+          console.log("recorder started");
+        })
+        .mouseup(function() {
+          console.log("rec end");
+          $(".rec-button i").removeClass("recStart");
+          mediaRecorder.stop();
+          mediaRecorder.ondataavailable = function(e) {
+            chunks.push(e.data);
+            var blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+            var player = document.getElementById("player");
+            player.src = URL.createObjectURL(blob);
+            chunks = [];
+            var reader = new window.FileReader();
+            reader.readAsDataURL(blob);
+            reader.onloadend = function() {
+              var base64 = reader.result;
+              var audioArr = {
+                audio: base64
+              };
+              $.ajax({
+                url: "http://localhost:4242/api/1.0/post",
+                type: "POST",
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify(audioArr),
+                success: function(response) {
+                  console.log(response);
+                },
+                error: function(err) {
+                  console.log(err);
+                }
+              });
+            };
+            console.log(mediaRecorder.state);
+            console.log("recorder stopped");
+          };
+        });
+    })
+
+    // Error callback
+    .catch(function(err) {
+      console.log("The following getUserMedia error occured: " + err);
+    });
   $(".rcw-widget-container").addClass("rcw-opened");
   $(".rcw-launcher").trigger("click");
   $(".rcw-close-button").remove();
   $(".rcw-launcher").remove();
+  $(".rcw-sender").prepend(
+    $('<button class="rec-button">').append($('<i class="fas fa-microphone">'))
+  );
 });
-
-const styles = {
-  bubbleStyles: {
-    text: {
-      fontSize: 16
-    },
-    chatbubble: {
-      borderRadius: 30,
-      padding: 10
-    }
-  },
-  headerTitle: {
-    color: "white",
-    fontSize: 22
-  },
-  header: {
-    backgroundColor: "rgb(0, 132, 255)",
-    padding: 20,
-    borderTop: "12px solid rgb(204, 204, 204)"
-  },
-  messagesContainer: {
-    display: "flex",
-    flexDirection: "column",
-    padding: 10,
-    alignItems: "center"
-  },
-  input: {
-    fontSize: 16,
-    padding: 10,
-    outline: "none",
-    width: 350,
-    border: "none",
-    borderBottom: "2px solid rgb(0, 132, 255)"
-  }
-};
 
 export default App;
